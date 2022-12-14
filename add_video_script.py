59549c420b0f3579efcdb12fb0c675030148ede6
wjj <wjeffreyjohnston@gmail.com>
2022-12-14 10:31:22 -0800

    Added script for adding video to session_df

---
+++ add_video_script.py
@@ -0,0 +1,34 @@
+
+import argparse
+import os
+import pickle
+
+import conditioned_stimulus.auxiliary as csx
+
+def create_parser():
+    parser = argparse.ArgumentParser(description='fit several autoencoders')
+    parser.add_argument('data_file', help='path to session_df to use')
+    parser.add_argument('-o', '--output_file', default='session_df_video.pkl',
+                        type=str,
+                        help='path to save the output at')
+    parser.add_argument('--video_folder', default=None,
+                        help='path to video files -- will use "video_files" subfolder'
+                        ' of the folder holding the data if not supplied')
+    return parser
+
+if __name__ == '__main__':
+    parser = create_parser()
+    args = parser.parse_args()
+
+    data_file = args.data_file
+    if args.video_folder is None:
+        path, _ = os.path.split(data_file)
+        video_folder = os.path.join(path, 'video_files')
+    else:
+        video_folder = None
+
+    vs = csx.process_videos(video_folder)
+    data = csx.load_data(data_file, folder='')
+    data = csx.preprocess_data(data, video_data=vs)
+
+    pickle.dump(data, open(args.output_file, 'wb'))
